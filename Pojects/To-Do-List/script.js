e8bf300c8f27961a400f9f02f60de7b38369bb67
kunj-pandya <kunjpandya998@gmail.com>
2026-03-22 16:42:57 +0530

    Add: delete task

--- Pojects/To-Do-List/script.js
+++ Pojects/To-Do-List/script.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         tasks.push(newTask);
         saveTasks();
+        renderTask(newTask);
 
         todoInput.value = "";
         // console.log(tasks);
@@ -31,9 +32,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const li = document.createElement("li");
         li.setAttribute("data-id", task.id);
+        if (task.completed) li.classList.add("completed");
         li.innerHTML = ` 
         <span>${task.text}</span>
         <button>delete</button>`;
+
+        li.addEventListener("click", (e) => {
+            if (e.target.tagName == "BUTTON") return;
+            task.completed = !task.completed;
+            li.classList.toggle("completed");
+            saveTasks();
+        })
+
+        li.querySelector("button").addEventListener("click", (e) => {
+            e.stopPropagation();
+            tasks = tasks.filter(t => t.id !== task.id);
+            li.remove();
+            saveTasks();
+        })
 
         todoList.appendChild(li);
     }
