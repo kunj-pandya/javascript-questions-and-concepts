569a7aa591cdd226577944540ac4c344790c8519
kunj-pandya <kunjpandya998@gmail.com>
2026-03-22 16:27:38 +0530

    Add: renderTask function and Dom content loaded event

--- Pojects/To-Do-List/script.js
+++ Pojects/To-Do-List/script.js
@@ -1,27 +1,45 @@
-const todoInput = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+    const todoInput = document.getElementById("todo-input");
+    const addTaskButton = document.getElementById("add-task-btn");
+    const todoList = document.getElementById("todo-list");
 
-let tasks = [];
+    let saved = localStorage.getItem('task');
+    let tasks = saved ? JSON.parse(saved) : [];
 
-addTaskButton.addEventListener("click", () => {
-    const taskText = todoInput.value.trim();
+    tasks.forEach((task) => renderTask(task));
 
-    if (taskText === "") return;
+    addTaskButton.addEventListener("click", () => {
+        const taskText = todoInput.value.trim();
 
-    const newTask = {
-        id: Date.now(),
-        text: taskText,
-        completed: false
-    };
+        if (taskText === "") return;
 
-    tasks.push(newTask);
-    saveTasks();
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed: false
+        };
 
-    todoInput.value = "";
-    console.log(tasks);
+        tasks.push(newTask);
+        saveTasks();
+
+        todoInput.value = "";
+        // console.log(tasks);
+    });
+
+    function renderTask(task) {
+        // console.log(task.text);
+
+        const li = document.createElement("li");
+        li.setAttribute("data-id", task.id);
+        li.innerHTML = ` 
+        <span>${task.text}</span>
+        <button>delete</button>`;
+
+        todoList.appendChild(li);
+    }
+
+    function saveTasks() {
+        localStorage.setItem("task", JSON.stringify(tasks));
+    }
+
 });
-
-function saveTasks() {
-    localStorage.setItem("task", JSON.stringify(tasks));
-}
